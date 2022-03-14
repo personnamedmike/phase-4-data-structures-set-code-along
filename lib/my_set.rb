98436b55993ce01113bfe5b7668f0b91e62a2030
Mike Starr <imhs@protonmail.com>
2022-03-14 14:45:29 -0700

    commit tree da479e97ea47fa3892951bbeea1368b3a2a4992c

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,15 @@
 # your code here
+class MySet
+    def initialize(enumerable = [])
+      @hash = {}
+      enumerable.each do |value|
+        @hash[value] = true
+      end
+    end
+  end
+
+set = MySet.new([1,2,3,3])
+
+def include?(value)
+    @hash.has_key?(value)
+end
